20fcad4521741bd4ab0837e2a7e7e41abcf32b43
Mihir RameshChandra Bhatt <mihirbhatt000@gmail.com>
2026-04-02 21:09:12 -0600

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            ans = seen.get(target - num)
+            if ans is not None:
+                return [ans, i]
+            else:
+                seen[num] = i
+
